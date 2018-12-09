db635c4ab06e55110b563767b5b219beba260688
Simeon Nikolov <simeon.v.nikolov@gmail.com>
2018-12-09 18:36:03 +0200

    Implement LinkedList - 04. Stacks and Queues

---
+++ Lecture04-StacksAndQueues/src/ds/lab/prob01/LinkedList.java
@@ -0,0 +1,145 @@
+package ds.lab.prob01;
+import java.util.Iterator;
+
+public class LinkedList<E> implements Iterable<E> {
+
+	private class Node {
+		private E element;
+		private Node next;
+		private Node prev;
+		
+		public Node(E element, Node next, Node prev) {
+			this.element = element;
+			this.next = next;
+			this.prev = prev;
+		}
+		
+		public E getValue() {
+			return this.element;
+		}
+		
+		public Node getNext() {
+			return this.next;
+		}
+		
+		public void setNext(Node next) {
+			this.next = next;
+		}
+		
+		public Node getPrev() {
+			return this.prev;
+		}
+		
+		public void setPrev(Node prev) {
+			this.prev = prev;
+		}
+	}
+	
+	private LinkedList<E>.Node firstNode;
+	private LinkedList<E>.Node lastNode;
+	
+	public LinkedList() {
+		this.setSize(0);
+	}
+	
+    private int size;
+
+    public int size() {
+        return this.size;
+    }
+
+    private void setSize(int size) {
+        this.size = size;
+    }
+
+    public void addFirst(E item) {
+    	@SuppressWarnings({ "rawtypes", "unchecked" })
+		LinkedList<E>.Node newNode = new LinkedList.Node(item, null, null);
+        
+    	if (this.size == 0) {
+        	this.firstNode = newNode;
+        	this.lastNode = newNode;
+        } else {
+        	newNode.setNext(firstNode);
+        	firstNode.setPrev(newNode);
+        	this.firstNode = newNode;
+        }
+    	
+    	this.setSize(this.size + 1);
+    }
+
+    public void addLast(E item) {
+    	@SuppressWarnings({ "rawtypes", "unchecked" })
+		LinkedList<E>.Node newNode = new LinkedList.Node(item, null, null);
+    	
+        if (this.size == 0) {
+        	this.firstNode = newNode;
+        	this.lastNode = newNode;
+        } else {
+        	newNode.setPrev(lastNode);
+        	lastNode.setNext(newNode);
+        	this.lastNode = newNode;
+        }
+        
+        this.setSize(this.size + 1);
+    }
+
+    public E removeFirst() {
+    	E element = null;
+    	
+        if (this.size > 0) {
+        	element = this.firstNode.getValue();
+        	this.setSize(this.size - 1);
+        	
+        	if (this.size == 0) {
+        		this.firstNode = null;
+        		this.lastNode = null;
+        	} else {        		
+        		LinkedList<E>.Node nextNode = this.firstNode.getNext();
+        		nextNode.setPrev(null);
+        		this.firstNode = nextNode;
+        	}
+        }
+        
+        return element;
+    }
+
+    public E removeLast() {
+        E element = null;
+        
+        if (this.size > 0) {
+        	element = this.lastNode.getValue();
+        	this.setSize(this.size - 1);
+        	
+        	if (this.size == 0) {
+        		this.firstNode = null;
+        		this.lastNode = null;
+        	} else {
+        		LinkedList<E>.Node prevNode = this.lastNode.getPrev();
+        		prevNode.setNext(null);
+        		this.lastNode = prevNode;
+        	}
+        }
+        
+        return element;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return new Iterator<E>() {
+        	private LinkedList<E>.Node currentNode = firstNode;
+
+			@Override
+			public boolean hasNext() {
+				return currentNode != null && currentNode.getNext() != null;
+			}
+
+			@Override
+			public E next() {
+				this.currentNode = this.currentNode.getNext();
+				return currentNode.getValue();
+			}
+		};
+    }
+
+}
---
+++ Lecture04-StacksAndQueues/src/ds/lab/prob01/LinkedListTests.java
@@ -0,0 +1,122 @@
+package ds.lab.prob01;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class LinkedListTests {
+
+    @Test
+    public void addFirstShouldIncreaseCount() {
+        LinkedList<Integer> list = new LinkedList<>();
+
+        list.addFirst(1);
+
+        Assert.assertEquals(1, list.size());
+    }
+
+    @Test
+    public void addLastShouldIncreaseCount() {
+        LinkedList<Integer> list = new LinkedList<>();
+
+        list.addLast(1);
+
+        Assert.assertEquals(1, list.size());
+    }
+
+    @Test
+    public void addFirstShouldAddElement() {
+        LinkedList<Integer> list = new LinkedList<>();
+
+        list.addFirst(1);
+
+        for (Integer item : list) {
+            Assert.assertEquals(Integer.valueOf(1), item);
+        }
+    }
+
+    @Test
+    public void AddLastShouldAddElement() {
+        LinkedList<Integer> list = new LinkedList<>();
+
+        list.addLast(1);
+
+        for (Integer item : list) {
+            Assert.assertEquals(Integer.valueOf(1), item);
+        }
+    }
+
+    @Test
+    public void addFirstMultipleElementsShouldAddElement() {
+        LinkedList<Integer> list = new LinkedList<>();
+
+        for (int i = 0; i < 100; i++) {
+            list.addFirst(i);
+            int expected = i + 1;
+            Assert.assertEquals(expected, list.size());
+        }
+    }
+
+    @Test
+    public void addLastMultipleElementsShouldAddElement() {
+        LinkedList<Integer> list = new LinkedList<>();
+
+        for (int i = 0; i < 100; i++) {
+            list.addLast(i);
+            int expected = i + 1;
+            Assert.assertEquals(expected++, list.size());
+        }
+    }
+
+    @Test
+    public void removeFirstSingleElementShouldDecreaseCount() {
+        LinkedList<Integer> list = new LinkedList<>();
+
+        list.addFirst(1);
+        list.addFirst(2);
+        list.removeFirst();
+
+        Assert.assertEquals(1, list.size());
+    }
+
+    @Test
+    public void RemoveLastSingleElementShouldDecreaseCount() {
+        LinkedList<Integer> list = new LinkedList<>();
+
+        list.addFirst(1);
+        list.addFirst(2);
+        list.removeLast();
+
+        Assert.assertEquals(1, list.size());
+    }
+
+    @Test
+    public void RemoveFirstMultipleElementsShouldRemove() {
+        LinkedList<Integer> list = new LinkedList<>();
+
+        for (int i = 0; i < 100; i++) {
+            list.addLast(i);
+        }
+
+        for (Integer i = 0; i < 100; i++) {
+            Assert.assertEquals(i, list.removeFirst());
+        }
+
+        Assert.assertEquals(0, list.size());
+    }
+
+    @Test
+    public void RemoveLastMultipleElementsShouldRemove() {
+        LinkedList<Integer> list = new LinkedList<>();
+
+        for (int i = 0; i < 100; i++) {
+            list.addFirst(i);
+        }
+
+        for (Integer i = 0; i < 100; i++) {
+            Assert.assertEquals(i, list.removeLast());
+        }
+
+        Assert.assertEquals(0, list.size());
+    }
+
+}
